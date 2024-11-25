5fec3a725a964920e6a610c74e192d35127698a5
Athip888 <kamtangnaathip@gmail.com>
2024-11-25 14:22:22 +0700

    Add files via upload

---
+++ Project/php/logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start(); // เริ่มต้นเซสชัน
+
+// ลบข้อมูลทั้งหมดในเซสชัน
+session_unset();
+
+// ทำลายเซสชัน
+session_destroy();
+
+// เปลี่ยนเส้นทางไปยังหน้า login หรือหน้าแรกหลังจากออกจากระบบ
+header("Location: ../user/index.html");
+exit();
+?>
