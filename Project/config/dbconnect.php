b943cc2b421cc81d21bc4de06ff3d9188e867197
Athip888 <kamtangnaathip@gmail.com>
2025-02-01 22:39:57 +0700

    Add files via upload

---
+++ Project/config/dbconnect.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "udtechcar";
+$connect = mysqli_connect($servername, $username, $password, $db);
+
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "";
+
+?>
