aa4dd601a215ba2a444440087f374bc8bb7b14e5
MacJack5 <sirinarong.rueangsak@gmail.com>
2025-02-03 17:27:20 +0700

    Delete Project directory

--- Project/function/BuddhistYear.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ฟังก์ชันแปลงวันที่ (yyyy-mm-dd) จาก ค.ศ. เป็น พ.ศ.
-function convertDateToBuddhist($date) {
-    $dateObj = new DateTime($date);
-    $buddhistYear = $dateObj->format("Y") + 543;
-    return $dateObj->format("d/m/") . $buddhistYear;
-}
-?>
